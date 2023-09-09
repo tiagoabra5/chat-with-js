39daa973faf103a934336acc866c2539f74136e9
Tiago Abraão <125285649+tiagoabra5@users.noreply.github.com>
2023-09-08 22:43:34 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const responses = [
+    "Olá! Como posso ajudar?",
+    "Claro, estou aqui para ajudar!",
+    "Você tem alguma dúvida específica?",
+    // Adicione mais respostas conforme necessário
+];
 
+const chatWindow = document.querySelector(".chat-window");
+const chatForm = document.querySelector(".chat-form");
+const inputField = document.querySelector(".chat-form input");
+
+chatForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const userMessage = inputField.value;
+    displayMessage(userMessage, "user");
+    inputField.value = "";
+    setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * responses.length);
+        const responseMessage = responses[randomIndex];
+        displayMessage(responseMessage, "bot");
+    }, 500); // Simula resposta do atendente após 500ms
+});
+
+function displayMessage(message, sender) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("message", sender);
+    messageElement.textContent = message;
+    chatWindow.appendChild(messageElement);
+}
